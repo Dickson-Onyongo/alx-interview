bfe3cee8aad82be4575f43e453848bfd06994ba5
Dickson-Onyongo <dicksononyong@gmail.com>
2024-10-07 07:45:09 +0300

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A script to determine the Pascals Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of intergers representing the Pascals Triangle of n.
+    """
+    list = []
+    if n <= 0:
+        return list
+   # list.append([1])
+    for i in range(n):
+        temp_list = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(list[i-1][j-1] + list[i-1][j])
+        list.append(temp_list)
+    #print list(the triangle)
+    return list
