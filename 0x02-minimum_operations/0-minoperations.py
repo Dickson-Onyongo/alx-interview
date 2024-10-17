577f75058f6b20bd7247d04b0e3d3078ebb38659
Dickson-Onyongo <dicksononyong@gmail.com>
2024-10-18 00:35:25 +0300

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -19,17 +19,17 @@ def minOperations(n):
             clipboard = done
             done += clipboard
             ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
+            
         elif n - done > 0 and (n - done) % done == 0:
             # copy all and paste
             clipboard = done
             done += clipboard
             ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
+        
         elif clipboard > 0:
             # paste
             done += clipboard
             ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
-    # print('')
+
+
     return ops_count
